e137ac400676f095f0a51a6a8df46b86b24035f5
Diegoh-dev <diegoh.developer@gmail.com>
2023-07-14 21:20:13 -0300

    feat: Inicinado rota de autenticação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
-import { Header } from '@/components/header'
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+'use client'
+import { Header } from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Tarefas+ | Organize suas tarefas',
-  description: 'organize suas tarefas',
-}
+  title: "Tarefas+ | Organize suas tarefas",
+  description: "organize suas tarefas",
+};
 
 export default function RootLayout({
   children,
+  session,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
+  session:any;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-      <Header/>
-        {children}
+    <SessionProvider session={session}>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          {children}
         </body>
-    </html>
-  )
+      </html>
+    </SessionProvider>
+  );
 }
