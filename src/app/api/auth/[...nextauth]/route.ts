5d5d7ae1d3a3afb1ccdd0c4ef4bb047a9152f3a5
Diegoh-dev <diegoh.developer@gmail.com>
2023-07-16 14:03:19 -0300

    feat[login]: autenticação do usuario.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions = {
+export const authOption = {
   providers:[
     GoogleProvider({
       clientId:process.env.GOOGLE_CLIENT_ID as string,
@@ -11,4 +11,7 @@ export const authOptions = {
   secret:process.env.JWT_SECRET as string,
 }
 
-export default NextAuth(authOptions)
+ const handler = NextAuth(authOption);
+
+
+export {handler as GET ,handler as POST}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,49 @@
-import Link from 'next/link';
-import styles from './header.module.scss';
+"use client";
+import Link from "next/link";
+import styles from "./header.module.scss";
 
-export function Header(){
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+
+export function Header() {
+  const { data: session, status } = useSession();
+
   return (
     <header className={styles.header}>
       <section className={styles.content}>
         <nav className={styles.nav}>
-          <Link href='/'>
-          <h1 className={styles.logo}>Tarefas <span>+</span> </h1>
+          <Link href="/">
+            <h1 className={styles.logo}>
+              Tarefas <span>+</span>{" "}
+            </h1>
           </Link>
-          <Link href='/dashboard' className={styles.link}>
-         Meu Painel
-          </Link>
+          {status === "authenticated" && (
+            <Link href="/dashboard" className={styles.link}>
+              Meu Painel
+            </Link>
+          )}
         </nav>
-        <button className={styles.loginButton}>Acessar</button>
+        {status === "authenticated" ? (
+          <button
+            onClick={() => {
+              signOut();
+            }}
+            className={styles.loginButton}
+          >
+           Olá {session.user?.name}
+            {/* <Image width={20} height={20} src={session.user?.image as string} alt={session.user?.name as string}/> */}
+          </button>
+        ) : (
+          <button
+            onClick={(e) => {
+              signIn("google");
+            }}
+            className={styles.loginButton}
+          >
+            Acessar
+          </button>
+        )}
       </section>
     </header>
-  )
+  );
 }
