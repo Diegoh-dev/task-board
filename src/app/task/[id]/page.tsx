95438df398e2d7c1aed3ccdda95bb2256dd3e1c3
Diegoh-dev <diegoh.developer@gmail.com>
2023-09-08 12:38:12 -0300

    feat[task]: mostrando comentários e excluindo comentarios.

--- src/app/task/[id]/page.tsx
+++ src/app/task/[id]/page.tsx
@@ -8,10 +8,12 @@ import {
   query,
   where,
   getDoc,
-  addDoc
+  getDocs,
+  addDoc,
+  deleteDoc
 } from 'firebase/firestore';
 import { TextArea } from '@/components/textArea';
-
+import {FaTrash} from 'react-icons/fa'
 import {useState,ChangeEvent,FormEvent,useEffect} from 'react';
 import {useSession} from 'next-auth/react';
 
@@ -33,7 +35,19 @@ interface taskUnicaProps{
   user:string;
   taskId:string;
 }
-export default function Task({params}:TaskProps){
+
+interface ComentariosProps{
+  id:string;
+  comentario:string;
+  taskId:string;
+  user:string;
+  name:string;
+}
+export default function Task({params,}:TaskProps){
+  let allComentarios:ComentariosProps[] = [];
+
+  console.log({allComentarios})
+
   const {id} = params;
 
   const {data: session} = useSession();
@@ -45,10 +59,32 @@ export default function Task({params}:TaskProps){
     taskId:'',
   });
   const [comentarios,setComentarios] = useState('');
-  
-  console.log(comentarios);
-
+  const [allComentariosTaks,setAllComentariosTaks] = useState<ComentariosProps[]>(allComentarios || []);
   const docRef = doc(db,"tarefas",id);
+
+
+  const queryComentarios = query(collection(db,"comentarios"),where("taskid","==",id));
+
+ async function getComentarios(){
+   const snapshotComentarios  = await getDocs(queryComentarios);
+   console.log({snapshotComentarios})
+
+   snapshotComentarios.forEach((doc) => {
+    allComentarios.push({
+      id:doc.id,
+      comentario:doc.data().comentario,
+      user:doc.data().user,
+      name:doc.data().name,
+      taskId:doc.data().taskId
+    })
+   })
+
+  }
+  
+  useEffect(()=>{
+    getComentarios();
+  },[])
+
   useEffect(()=>{
     async function getSnap(){
       const snapshot = await getDoc(docRef);
@@ -76,7 +112,7 @@ export default function Task({params}:TaskProps){
     getSnap()
   },[])
 
-
+console.log({allComentariosTaks})
 
 
 
@@ -97,7 +133,18 @@ export default function Task({params}:TaskProps){
 
        })
 
-       setComentarios('')
+       setComentarios("");
+
+       const data = {
+        id:docRef.id,
+        comentario:comentarios,
+        taskId:tasks.taskId,
+        user:session.user.email,
+        name:session.user.name,
+       }
+//                            preveState = itens que existe no estado no momento.
+       setAllComentariosTaks((oldItens) => [...oldItens,data]);
+   
     } catch (error) {
       console.log(error);
     }
@@ -105,33 +152,70 @@ export default function Task({params}:TaskProps){
 
   console.log(tasks);
 
+  async function handleDeleteComment(id:string){
+    try {
+      const docRef = doc(db,"comentarios",id);
+      await deleteDoc(docRef);
+      alert('deletado');
+
+      const deletComentarios = allComentariosTaks.filter((item)=> item.id !== id);
+
+      setAllComentariosTaks(deletComentarios);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className={styles.container}>
-    <main className={styles.main}>
-    <h1>Tarefa</h1>
-    <article className={styles.task}>
-      <p>
-        {tasks?.tarefa}
-      </p>
-    </article>
-    </main>
+      <main className={styles.main}>
+        <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>{tasks?.tarefa}</p>
+        </article>
+      </main>
 
-    <section className={styles.commentscontainer}>
-      <h2>Deixar comentários</h2>
+      <section className={styles.commentscontainer}>
+        <h2>Deixar comentários</h2>
 
-      <form action="" onSubmit={handleComment} >
-        <TextArea 
-        placeholder='Digite seu comentário...'
-        value={comentarios}
-        onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setComentarios(e.target.value)}
-        />
-        <button className={styles.button}
-        disabled={!session?.user}
-        >
-          Enviar
-        </button>
-      </form>
-    </section>
+        <form action="" onSubmit={handleComment}>
+          <TextArea
+            placeholder="Digite seu comentário..."
+            value={comentarios}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+              setComentarios(e.target.value)
+            }
+          />
+          <button className={styles.button} disabled={!session?.user}>
+            Enviar
+          </button>
+        </form>
+      </section>
+
+      <section className={styles.commentscontainer}>
+        <h2>Todos os comentários</h2>
+        {allComentariosTaks.length === 0 && (
+          <span>Nenhum comentário foi encontrado...</span>
+        )}
+
+        {allComentariosTaks.map((item) => (
+          <article key={item.id} className={styles.comment}>
+            <div className={styles.headComment}>
+              <label className={styles.commentLabel}>{item.name}</label>
+              {item.user === session?.user?.email && (
+                <button className={styles.buttonTrash}
+                onClick={()=> {
+                  handleDeleteComment(item.id);
+                }}
+                >
+                  <FaTrash size={18} color="#EA3140" />
+                </button>
+              )}
+            </div>
+            <p>{item?.comentario}</p>
+          </article>
+        ))}
+      </section>
     </div>
-  )
+  );
 }
