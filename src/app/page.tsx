36e36dea49abfe28b6c2d9747f4554914c07f790
Diegoh-dev <diegoh.developer@gmail.com>
2023-09-14 22:59:25 -0300

    armazenamento em cache e revalidação de dados.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,46 @@ import Image from 'next/image';
 import styles from './page.module.scss';
 import hero from '../../public/assets/hero.png';
 import { Roboto } from 'next/font/google';
+import {
+  collection,
+  getDocs
+} from 'firebase/firestore';
+
+import {db} from '../services/firebaseConnection';
+import {cache} from 'react'; //A função React cacheé usada para memorizar solicitações de dados.
+
+export const revalidate = 3600 // revalidate the data at most every hour
+ 
 
 const roboto = Roboto({weight:['400','500','700'],subsets:['latin']})
 
-export default function Home() {
+export default async function Home() {
+
+  // async function getSize(){
+  //   const comentarios = collection(db,'comentarios');
+  //   const posts = collection(db,'tarefas');
+  
+  //   const comentariosSnapshot = await getDocs(comentarios);
+  //   const postSnapshot = await getDocs(posts);
+
+   
+
+  //   const result = {
+  //     posts:postSnapshot.size || 0,
+  //     comentarios: comentariosSnapshot.size || 0
+  //   }
+  
+  //   // return {
+  //   //   posts:postSnapshot.size || 0,
+  //   //   comentarios: comentariosSnapshot.size || 0
+  //   // }
+  // // }
+
+  const result = await getSize();
+
+  // console.log('result:',result)
+
+
   return (
     <main className={`${roboto.className} ${styles.main}`}>
       <div className={styles.containerHero}>
@@ -21,10 +57,10 @@ export default function Home() {
 
         <div className={styles.containerSection}>
         <section className={styles.sectionsDados}>
-          +7mil posts
+          +{result.posts} posts
         </section>
         <section className={styles.sectionsDados}>
-          +1mil comentários
+          +{result.comentarios} comentários
         </section>
       </div>
       </div>
@@ -33,3 +69,17 @@ export default function Home() {
     </main>
   )
 }
+
+
+export const getSize = cache(async () => {
+  const comentarios = collection(db, "comentarios");
+  const posts = collection(db, "tarefas");
+
+  const comentariosSnapshot = await getDocs(comentarios);
+  const postSnapshot = await getDocs(posts);
+
+  return {
+    posts: postSnapshot.size || 0,
+    comentarios: comentariosSnapshot.size || 0,
+  };
+});
